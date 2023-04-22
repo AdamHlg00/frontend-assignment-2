a3168ebc61f1541ad771b6e24428290ccfe0b191
AdamHlg00 <adamoliver.holgersson@gmail.com>
2023-04-22 11:32:12 +0200

    Added some utility functions from lectures.

---
+++ src/utilities/generate-booking-number.js
@@ -0,0 +1,12 @@
+export function generateBookingNumber() {
+  let no = ''
+  while (no.length < 3) {
+    no += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[Math.floor(Math.random() * 26)]
+  }
+
+  while (no.length < 6) {
+    no += Math.floor(Math.random() * 10)
+  }
+
+  return no
+}
