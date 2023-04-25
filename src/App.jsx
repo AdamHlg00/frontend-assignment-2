ee09c8e130b161cd045e405782f74fb43d9e7210
AdamHlg00 <adamoliver.holgersson@gmail.com>
2023-04-25 13:50:32 +0200

    Individual screenings are now displayed instead of movies. Information about movies are mapped to their respective screenings.

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,23 @@ import { kebabify } from './utilities/kebabify'
 
 export default function App() {
   const s = useStates('main', {
-    movies: []
+    movies: [],
+    screenings: [],
+    sorting: 'Date (Ascending)'
   })
 
   useEffect(() => {
     (async () => {
       let movies = await (await fetch('/api/movies')).json()
+      console.log(movies)
       for (let movie of movies) {
         movie.slug = kebabify(movie.title)
       }
       s.movies = movies
+
+      let screenings = await (await fetch('/api/screenings')).json()
+      console.log(screenings)
+      s.screenings = screenings
     })()
   }, [])
 
--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -4,14 +4,44 @@ import { Link } from 'react-router-dom'
 export default function MovieList() {
   const s = useStates('main')
 
+  const handleSortingOption = (event) => {
+    s.option = event.target.value
+    console.log(s.option)
+  }
+
   return <>
-    {s.movies.map(({ slug, title, description }) => <Link
+    <div>
+      <label><span>Sort by:</span>
+        <select className="sortOption" onChange={handleSortingOption}>
+          <option>Date (Ascending)</option>
+          <option>Date (Descending)</option>
+        </select>
+      </label>
+    </div>
+
+    {/*s.movies.map(({ slug, title, description }) => <Link
       to={'/movie-detail/' + slug}>
       <div className="movie">
         <h3>{title}</h3>
         <img src={'https://cinema-rest.nodehill.se' + description.posterImage} />
       </div>
     </Link>
-    )}
+)*/}
+
+    {s.screenings.map(({ id, time, movieId }) => {
+      const movie = s.movies.find(({ id }) => id === movieId)
+      return (
+        <div className="screening" key={id}>
+          <h3>{time}</h3>
+          {movie && (
+            <div className="movie">
+              <h4>{movie.title}</h4>
+              <img src={`https://cinema-rest.nodehill.se${movie.description.posterImage}`} />
+            </div>
+          )}
+        </div>
+      )
+    })}
   </>
 }
+
